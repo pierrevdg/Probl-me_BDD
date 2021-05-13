8136a72552dde531e80b88dd79b80cb93290cae7
Pierre VANDERGOOTEN <pierre.vandergooten@edu.devinci.fr>
2021-05-13 11:42:33 +0200

    Ajoutez des fichiers projet.

---
+++ Problème/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace Problème
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Ouverture de connexion
+
+            MySqlConnection maConnexion = null;
+            try
+            {
+                string connexionString = "SERVER=localhost;PORT=3306;" +
+                                         "DATABASE=veloMax;" +
+                                         "UID=root;PASSWORD=Ropie?!mlb78"; //met ton mot de passe si tu veux accéder à ta bdd sql
+                                         // faudra d'ailleurs qu'on ajoute le mot de passe root pour rendre le problème
+
+                maConnexion = new MySqlConnection(connexionString);
+                maConnexion.Open();
+            }
+            catch (MySqlException e)
+            {
+                Console.WriteLine(" ErreurConnexion : " + e.ToString());
+                return;
+            }
+            #endregion
+        }
+    }
+}
